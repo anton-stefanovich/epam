886f3b2bdd5840da6181220290222cf7ebe8434b
Unknown <anton.stefanovich@gmail.com>
2018-10-08 09:42:07 -0400

    + Path builder added

--- test.py
+++ test.py
@@ -1,13 +1,58 @@
 from random import randint
 
+figures_next = {
+    'bishop': {
+        'vectors': [
+            (1, 1), (-1, -1),
+            (1, -1), (-1, 1)],
+        'multipliable': True,
+    },
+
+    'rook': {
+        'vectors': [
+            (1, 0), (-1, 0),
+            (0, 1), (0, -1)],
+        'multipliable': True,
+    },
+
+    'knight': {
+        'vectors': [
+            (2, 1), (2, -1),
+            (-2, 1), (-2, -1),
+            (1, 2), (1, -2),
+            (-1, 2), (-1, -2)],
+        'multipliable': False,
+    },
+
+    'king': {
+        'vectors': [
+            (1, 0), (-1, 0),
+            (0, 1), (0, -1),
+            (1, 1), (-1, -1),
+            (-1, 1), (1, -1)],
+        'multipliable': False,
+    },
+
+    'queen': {
+        'vectors': [
+            (1, 0), (-1, 0),
+            (0, 1), (0, -1),
+            (1, 1), (-1, -1),
+            (-1, 1), (1, -1)],
+        'multipliable': True,
+    },
+}
+
 figures = {
     'queen': [
         # 0:
         [8, 5, 2],
+        # [7, 9, 8, 5, 2],
         # 1:
         [2, 3, 4, 7, 5, 9],
         # 2:
         [1, 3, 5, 8, 0],
+        # [1, 3, 5, 8, 0, 4, 6],
         # 3:
         [2, 1, 6, 9, 5, 7],
         # 4:
@@ -49,20 +94,101 @@ figures = {
 }
 
 
+# rebuild available matrix for the field
+def build_matrix(field, vectors, multipliable):
+    available_matrix = list(range(10))
+
+    for y in range(len(field)):
+        for x in range(len(field[y])):
+            point = (x, y)
+            if not is_point_valid(field, point):
+                continue
+
+            available_points = []
+            for vector in vectors:
+                available_points.extend(
+                    get_available_points(
+                        field, (x, y), vector, multipliable))
+
+            available_cells = []
+            for point in available_points:
+                available_cells.append(
+                    int(field[point[1]][point[0]]))
+
+            available_matrix[
+                int(field[y][x])] = available_cells
+
+    return available_matrix
+
+
+# get available point from vector and multipliable property
+# start from one point, return all available points
+def get_available_points(field, point, vector, multipliable):
+    new_point = get_next_point(point, vector)
+
+    if not is_point_valid(field, new_point):
+        return list()
+
+    else:
+        next_points = \
+            get_available_points(
+                field, new_point, vector, multipliable) \
+            if multipliable else list()
+
+        next_points.append(new_point)
+        return next_points
+
+
+# get one next point based on current point and vector
+def get_next_point(point, vector):
+    return (point[0] + vector[0],
+            point[1] + vector[1])
+
+
+# check point validity
+def is_point_valid(field, point):
+    x, y = point[0], point[1]
+
+    return 0 <= y < len(field) \
+        and 0 <= x < len(field[y]) \
+        and field[y][x].isdigit()
+
+
 # 1   2   3
 # 4   5   6
 # 7   8   9
 # *   0   #
 
-def main():
-    start_point = randint(0, 9)
+# initial function for rebuild movements matrix
+def rebuild_matrix():
+    figures.clear()  # !!!!!!!!!
+
+    field = [
+        ['1', '2', '3'],
+        ['4', '5', '6'],
+        ['7', '8', '9'],
+        ['*', '0', '#'],
+    ]
+
+    for figure in figures_next:
+        figures[figure] = \
+            build_matrix(
+                field,
+                figures_next.get(figure).get('vectors'),
+                figures_next.get(figure).get('multipliable'))
+
+    print('=== Matrix rebuild completed ===')
+
+
+# calculate all path for all available figures
+def main(start_point):
     print('Start point: %s' % start_point)
 
     for figure in figures:
         seq = magic_func(
             [start_point],  # current array
             figure,         # current figure
-            3,              # max path length
+            7,              # max path length
         )
 
         print('Figure: \'%s\', ways: %d' % (figure, len(seq)))
@@ -72,6 +198,8 @@ def main():
         #     print(path)
 
 
+# collecting all available path
+# step by step (recursively)
 def magic_func(current, figure, length):
     results = []
     for next_pos in figures.get(figure)[current[-1]]:
@@ -86,5 +214,18 @@ def magic_func(current, figure, length):
     return results
 
 
+# enter point
 if __name__ == "__main__":
-    main()
+    # generate random start point
+    start_here = randint(0, 9)
+
+    # launch path calculation
+    # based on manual path matrix
+    main(start_here)
+
+    # rebuilding path matrix
+    rebuild_matrix()
+
+    # launch path calculation
+    # based on generated path matrix
+    main(start_here)
